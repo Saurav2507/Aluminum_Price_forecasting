ab2efab8791fb7a2293a05faa2e4f265dcf12dd9
Saurav2507 <sauravshinde2507@gmail.com>
2025-02-25 14:20:47 +0530

    app.py

--- app.py.py
+++ app.py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1EzjG15VSUtQ0r6EtU06xxSeNQ7oqr8l6
 """
 
-!pip install streamlit
+
 import streamlit as st
 import pandas as pd
 import datetime
